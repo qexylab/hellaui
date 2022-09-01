6a3c3272cc7b63c0acf7ed2a1ee370811355fa63
Shuffle5 <shuffl35@yandex.ru>
2022-09-01 10:07:44 +0300

    update

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@/src': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+    },
+    sourcemap: true,
+    target: ['ESNext'],
+  },
+  plugins: [react()],
+});
