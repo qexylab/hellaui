4e17cac01d5567ad002e1d0bee24555675b1aa9e
Shuffle5 <shuffl35@yandex.ru>
2022-09-01 10:29:52 +0300

    update

---
+++ src/index.test.ts
@@ -0,0 +1,11 @@
+import * as HellaUI from './index'
+
+/**
+ * Looks a bit of a silly test, however this ensures that we don't accidentally expose something to
+ * the outside world that we didn't want!
+ */
+it('should expose the correct components', () => {
+  expect(Object.keys(HellaUI)).toEqual([
+    'Button',
+  ])
+})
