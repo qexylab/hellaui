183e5368473125e155bdb5c675fd1fada8a15431
Shuffle-5 <0870989@gmail.com>
2022-09-09 12:32:39 +0300

    Fixed check for tests

--- src/components/utils/useSetAnimation.ts
+++ src/components/utils/useSetAnimation.ts
@@ -4,11 +4,11 @@ export const useSetAnimation = (
     animationName: string = '',
     keyframe: string = '',
 ) => {
-  // If the script is launched using a test, then it will skip this hook
-  if (typeof window !== 'undefined') return
 
   useEffect(() => {
     const styleSheet = document.styleSheets[0]
+    // If the script is launched using a test, then it will skip this hook
+    if (typeof styleSheet === 'undefined') return
     styleSheet.insertRule(keyframe, styleSheet.cssRules.length)
   }, [])
 }
