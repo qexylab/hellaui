920287830b889adb1b65b7048bfac2112e8709d4
Shuffle-5 <0870989@gmail.com>
2022-09-19 16:17:08 +0300

    add first version radio-button

--- src/components/radioButton/RadioButton.types.ts
+++ src/components/radioButton/RadioButton.types.ts
@@ -3,9 +3,14 @@ import { HTMLAttributes, ReactNode } from 'react'
 export type RadioButtonSize = 'xl' | 'lg' | 'md' | 'sm' | 'xs'
 
 export interface IRadioButton extends HTMLAttributes<HTMLInputElement> {
+  inputId: string // Input id
   checked?: boolean // Checked radioButton state
   size?: RadioButtonSize // RadioButton size
   disabled?: boolean // Disable radioButton
   tooltipText?: ReactNode // Tooltip at the bottom of the radioButton
   error?: boolean // Error state
+  required?: boolean // Required state
+  inputRef?: HTMLInputElement // Input ref
+  value: string // Input value
+  name: string // Input name
 }
--- src/components/radioButton/index.tsx
+++ src/components/radioButton/index.tsx
@@ -1,4 +1,4 @@
-import React, { ForwardedRef, forwardRef, useState } from 'react'
+import React, {ChangeEvent, FormEvent, ForwardedRef, forwardRef, useEffect, useRef, useState} from 'react'
 import { IRadioButton } from '@src/components/radioButton/RadioButton.types'
 import { theme_color } from '@src/components/theme'
 import { getRadioButtonStyle } from '@src/components/radioButton/RadioButton.style'
@@ -8,138 +8,221 @@ export const RadioButton = forwardRef<HTMLInputElement, IRadioButton>(
   (
     {
       children,
+      id,
+      inputId,
+      className,
       size = 'md',
-      checked,
+      checked = false,
       error = false,
-      disabled,
+      disabled = false,
       tooltipText,
+      required = false,
+      inputRef = null,
+      value,
+      name,
+      onChange,
       ...props
     },
     ref: ForwardedRef<HTMLInputElement>
   ) => {
-    const { minWidth, height, paddingLeft } = getRadioButtonStyle(size)
-    const [isHover, setIsHover] = useState<boolean>(false)
-    const [isFocus, setIsFocus] = useState<boolean>(false)
+    // const { minWidth, height, paddingLeft } = getRadioButtonStyle(size)
+    // const [isHover, setIsHover] = useState<boolean>(false)
+    // const [isFocus, setIsFocus] = useState<boolean>(false)
 
-    useSetStyle('hl-radio:not(:checked):disabled', {
-      backgroundColor: theme_color.white_gray
-    })
-    useSetStyle('hl-radio:checked:disabled', {
-      border: `1px solid ${theme_color.dark_primary}`
-    })
-    useSetStyle('hl-radio:checked:not(disabled)', {
-      border: `1px solid ${theme_color.primary}`
-    })
-    useSetStyle('hl-radio:focus-visible', {
-      outlineOffset: '2px',
-      outline: `${theme_color.primary_40} solid 2px`
-    })
-    useSetStyle(
-      'hl-radio:not(:disabled):hover::after, hl-radio:not(:disabled):focus::after',
-      {
-        outlineOffset: '2px',
-        outline: `${theme_color.primary_40} solid 2px`,
-        content: '',
-        position: 'absolute',
-        left: '50%',
-        top: '50%',
-        transform: 'translate(-50%, -50%)',
-        borderRadius: '50%',
-        width: 'calc(100% + 16px)',
-        height: 'calc(100% + 16px)',
-        backgroundColor: `${theme_color.gray}`
+    const [focusedState, setFocusedState] = useState<boolean>(false)
+    const elementRef = useRef<HTMLDivElement>(null)
+    const _inputRef = useRef<HTMLInputElement>(inputRef)
+
+    const onClick = (e: ChangeEvent<HTMLInputElement>) => {
+      if (!disabled && onChange) {
+        const _checked = checked
+        const radioClicked = e.target instanceof HTMLDivElement
+        // @ts-ignore
+        const inputClicked = e.target === _inputRef.current
+        const isInputToggled = inputClicked && e.target.checked !== _checked
+        const isRadioToggled = radioClicked && !e.target.checked
+
+        if (isInputToggled || isRadioToggled) {
+          const _value = !_checked
+
+          onChange({
+            originalEvent: e,
+            value: value,
+            checked: _value,
+            stopPropagation: () => {},
+            preventDefault: () => {},
+            target: {
+              // @ts-ignore
+              type: 'radio',
+              name: name,
+              id: id,
+              value: value,
+              checked: _value
+            }
+          })
+        }
       }
-    )
-    useSetStyle(
-      'hl-radio:not(:disabled):hover, hl-radio:not(:disabled):focus',
-      { border: `1px solid ${theme_color.primary}` }
-    )
-
+    }
+    useEffect(() => {
+      if (_inputRef.current) _inputRef.current.checked = checked
+    }, [checked])
     return (
-      <label
-        style={{
-          margin: 0,
-          paddingTop: 2,
-          paddingBottom: 2,
-          paddingLeft: paddingLeft,
-          display: 'flex',
-          alignItems: 'center',
-          position: 'relative',
-          cursor: disabled ? 'default' : 'pointer',
-          color: disabled ? theme_color.gray : theme_color.danger
-        }}
-      >
+      <>
+        <style>{`
+        
+.hl-radiobutton-box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.hl-radiobutton-icon {
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  transform: translateZ(0) scale(.1);
+  border-radius: 50%;
+  visibility: hidden;
+}
+
+.hl-radiobutton-box.hl-highlight .hl-radiobutton-icon {
+  transform: translateZ(0) scale(1.0, 1.0);
+  visibility: visible;
+}
+
+.hl-radiobutton {
+  width: 20px;
+  height: 20px;
+}
+.hl-radiobutton .hl-radiobutton-box {
+  border: 2px solid #757575;
+  background: #ffffff;
+  width: 20px;
+  height: 20px;
+  color: rgba(0, 0, 0, 0.87);
+  border-radius: 50%;
+  transition: background-color 0.2s, border-color 0.2s, color 0.2s, box-shadow 0.2s, background-size 0.2s cubic-bezier(0.64, 0.09, 0.08, 1);
+}
+.hl-radiobutton .hl-radiobutton-box:not(.hl-disabled):not(.hl-highlight):hover {
+  border-color: rgba(0, 0, 0, 0.87);
+}
+.hl-radiobutton .hl-radiobutton-box:not(.hl-disabled).hl-focus {
+  outline: 0 none;
+  outline-offset: 0;
+  box-shadow: none;
+  border-color: #3F51B5;
+}
+.hl-radiobutton .hl-radiobutton-box .hl-radiobutton-icon {
+  width: 10px;
+  height: 10px;
+  transition-duration: 0.2s;
+  background-color: #3F51B5;
+}
+.hl-radiobutton .hl-radiobutton-box.hl-highlight {
+  border-color: #3F51B5;
+  background: #ffffff;
+}
+.hl-radiobutton .hl-radiobutton-box.hl-highlight:not(.hl-disabled):hover {
+  border-color: #3F51B5;
+  background: #ffffff;
+  color: #3F51B5;
+}
+.hl-radiobutton.hl-invalid > .hl-radiobutton-box {
+  border-color: #B00020;
+}
+.hl-radiobutton:focus {
+  outline: 0 none;
+}
+
+.hl-input-filled .hl-radiobutton .hl-radiobutton-box {
+  background-color: #f5f5f5;
+}
+.hl-input-filled .hl-radiobutton .hl-radiobutton-box:not(.hl-disabled):hover {
+  background-color: #ececec;
+}
+.hl-input-filled .hl-radiobutton .hl-radiobutton-box.hl-highlight {
+  background: #ffffff;
+}
+.hl-input-filled .hl-radiobutton .hl-radiobutton-box.hl-highlight:not(.hl-disabled):hover {
+  background: #ffffff;
+}
+
+.hl-radiobutton:not(.hl-radiobutton-disabled):hover {
+  box-shadow: 0 0 1px 10px rgba(0, 0, 0, 0.04);
+}
+.hl-radiobutton:not(.hl-radiobutton-disabled).hl-radiobutton-focused {
+  box-shadow: 0 0 1px 10px rgba(0, 0, 0, 0.12);
+}
+.hl-radiobutton.hl-radiobutton-checked:not(.hl-radiobutton-disabled):hover {
+  box-shadow: 0 0 1px 10px rgba(63, 81, 181, 0.04);
+}
+.hl-radiobutton.hl-radiobutton-checked:not(.hl-radiobutton-disabled).hl-radiobutton-focused {
+  box-shadow: 0 0 1px 10px rgba(63, 81, 181, 0.12);
+}
+
+.hl-hidden-accessible {
+  border: 0;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+}
+
+.hl-hidden-accessible input,
+.hl-hidden-accessible select {
+  transform: scale(0);
+}
+
+.hl-radiobutton-box.hl-highlight .hl-radiobutton-icon {
+  transform: translateZ(0) scale(1.0, 1.0);
+  visibility: visible;
+}
+
+        `}</style>
         <div
+          ref={elementRef}
+          id={id}
           style={{
-            position: 'absolute',
-            top: '2px',
-            left: '2px',
-            display: 'inline-block',
-            minWidth: minWidth,
-            height: height
+            display: 'inline-flex',
+            cursor: 'pointer',
+            userSelect: 'none',
+            verticalAlign: 'bottom'
           }}
+          className={`hl-radiobutton ${checked && 'hl-radiobutton-checked'}
+         ${disabled && 'hl-radiobutton-disabled'} ${
+            focusedState && 'hl-radiobutton-focused'
+          } ${className}`}
         >
-          <input
-            ref={ref}
-            onMouseEnter={() => setIsHover(true)}
-            onMouseLeave={() => setIsHover(false)}
-            onFocus={() => setIsFocus(true)}
-            onBlur={() => setIsFocus(false)}
-            type="radio"
-            className="hl-radio"
-            style={{
-              appearance: 'none',
-              width: '100%',
-              height: '100%',
-              position: 'absolute',
-              margin: 0,
-              padding: 0,
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              cursor: 'pointer',
-              borderRadius: '50%',
-              pointerEvents: disabled ? 'none' : 'auto'
-            }}
-          />
-          <span
-            style={{
-              display: 'inline-block',
-              position: 'absolute',
-              margin: 0,
-              left: 0,
-              top: 0,
-              right: 0,
-              bottom: 0,
-              content: '',
-              pointerEvents: 'none',
-              backgroundColor: theme_color.gray,
-              width: '20px',
-              height: '20px',
-              border: `1px solid ${
-                disabled
-                  ? theme_color.white_gray
-                  : error
-                  ? theme_color.danger
-                  : theme_color.dark_danger
-              }`,
-              borderRadius: '50%',
-              transition: 'all 0.25s ease-in-out'
-            }}
-          />
-        </div>
-        {children}
-        {tooltipText && (
-          <div
-            style={{
-              marginTop: 10,
-              color: disabled ? theme_color.dark_gray : theme_color.white_gray
-            }}
-          >
-            {tooltipText}
+          <div className='hl-hidden-accessible'>
+            <input
+                // @ts-ignore
+              ref={_inputRef}
+                // @ts-ignore
+              onClick={onClick}
+              id={inputId}
+              type="radio"
+              name={name}
+              defaultChecked={checked}
+              onFocus={() => setFocusedState(true)}
+              onBlur={() => setFocusedState(false)}
+                // @ts-ignore
+              onKeyDown={event => {if (event.code === 'Space') onClick(event)}}
+              disabled={disabled}
+              required={required}
+              tabIndex={props.tabIndex}
+            />
           </div>
-        )}
-      </label>
+          <div
+            className={`hl-radiobutton-box ${checked && 'hl-highlight'} ${
+              disabled && 'hl-disabled'
+            } ${focusedState && 'hl-focus'}`}
+          >
+            <div className="hl-radiobutton-icon" />
+          </div>
+        </div>
+      </>
     )
   }
 )
