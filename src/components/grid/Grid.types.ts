09cbc47ebf55bc2334b4dc999881e6d26d2e6237
Shuffle-5 <0870989@gmail.com>
2022-10-20 21:29:01 +0300

    fix

--- src/components/grid/Grid.types.ts
+++ src/components/grid/Grid.types.ts
@@ -4,5 +4,5 @@ export interface IGrid extends HTMLAttributes<HTMLDivElement> {
   columns?: number // The number of columns. Default - 12
   columnSpacing?: number | string // Defines the horizontal space between the type `children` components
   container?: boolean // You should be wrapping children with a container.
-  direction: 'row' | 'column' | 'column-reverse' | 'row-reverse'
+  direction?: 'row' | 'column' | 'column-reverse' | 'row-reverse'
 }
