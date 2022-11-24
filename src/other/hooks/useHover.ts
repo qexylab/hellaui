c5efef5f2ace98e85a926c6d0bfd97b475195580
Shuffle-5 <0870989@gmail.com>
2022-11-24 22:21:32 +0300

    Create useHover.ts

---
+++ src/other/hooks/useHover.ts
@@ -0,0 +1,45 @@
+import {MutableRefObject, useEffect, useRef, useState} from "react";
+
+interface Option {
+  onEnter?: () => void;
+  onLeave?: () => void;
+}
+
+export const useHover = <T extends HTMLElement>(targetRef: T, option: Option): boolean => {
+
+  const { onEnter, onLeave } = option || {}
+
+  const onEnterRef = useRef(onEnter);
+  const onLeaveRef = useRef(onLeave);
+  onEnterRef.current = onEnter;
+  onLeaveRef.current = onLeave;
+
+  const [hoverState, setHoverState] = useState<boolean>(false)
+
+  useEffect(() => {
+    const onMouseEnter = () => {
+      if (onEnterRef.current) onEnterRef.current()
+      setHoverState(true)
+    }
+    const onMouseLeave = () => {
+      if (onLeaveRef.current) onLeaveRef.current()
+      setHoverState(false)
+    }
+    if (!targetRef) return
+    targetRef.addEventListener('mouseenter', onMouseEnter)
+    targetRef.addEventListener('mouseleave', onMouseLeave)
+    return () => {
+      targetRef.removeEventListener('mouseenter', onMouseEnter);
+      targetRef.removeEventListener('mouseleave', onMouseLeave);
+    }
+  }, [])
+
+  return hoverState
+}
+
+const ref = useRef<HTMLSelectElement>(null)
+
+const hover = useHover(ref, {
+  onEnter: () => {},
+  onLeave: () => {}
+})
